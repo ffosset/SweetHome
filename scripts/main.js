6e326c2d59a771b4782937dfb6a560177e2f3923
ffosset <flora.fosset@gmail.com>
2017-06-17 12:51:31 +0200

    change lat lon barcelona

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ var mymap;
 var current_city;
 var cityLookup = {
 		    'Madrid':[40.45, -3.76, 11],
-		    'Barcelona':[41.38, 2.10, 11.5]
+		    'Barcelona':[41.365, 2.10, 11.5]
 };
 
 function calculateColor(percentage) {
